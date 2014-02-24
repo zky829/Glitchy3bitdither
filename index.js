dc3e5415cb04fb4e4527c059e9f4c3e7eca4afd8
JKirchartz <jkirchartz@gmail.com>
2014-02-24 05:27:11 +0100

    broken node.js app - but hey it's something

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-var Glitchy3bitdither = require("lib/Glitchy3bitdither.js").GLITCH;
+var Glitchy3bitdither = require("./lib/GlitchyDither.js").GLITCH;
+
+console.log(Glitchy3bitdither("seqGlitch"));
