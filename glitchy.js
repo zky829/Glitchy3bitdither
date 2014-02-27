02db67092fd97d236ed2df81ea1475a36561d1c6
JKirchartz <jkirchartz@gmail.com>
2014-02-27 11:06:48 +0100

    still can't get this to work

--- glitchy.js
+++ glitchy.js
@@ -4,30 +4,32 @@
  * Module dependencies.
  */
 
-var program = require('commander');
-var fs = require('fs');
+var program = require("commander");
+var fs = require("fs");
 var Glitchy3bitdither = require("./lib/GlitchyDither.js").GLITCH;
 
 program
   .version(require('./package.json').version)
-  .option('-f, --file', 'Input File')
-  .option('-o, --output', 'Output File')
-  .option('-g, --glitch', 'A glitch function, or list of glitch functions, to apply to an image/file')
   .option('-x, --experimental', 'Use experimental functions (optional)')
-  .option('-l, --list', 'list available functions');
+  .option('-l, --list', 'list available functions')
+  .option('-f, --file <file>', 'Input File')
+  .option('-o, --output <file>', 'Output File')
+  .option('-g, --glitch <name>', 'A glitch function, or list of glitch functions, to apply to an image/file');
 
 program.on('--help', function(){
   console.log('');
   console.log('Fork on Github @ http://github.com/JKirchartz/Glitchy3bitdither'); // more helpful info here, idk if i'll need it w/ the --list function.
 });
 
-if(program.list){
-     Glitchy3bitdither()(program); 
-}
+
+program.on('--list', function(){
+    console.log("list");
+     Glitchy3bitdither(program.args);
+});
+
 if(program.file && program.output){
      program.file = fs.readFile(program.file);
-     fs.writeFile(Glitchy3bitdither()(program));
+     fs.writeFile(Glitchy3bitdither(program.args));
 }
-
 
 program.parse(process.argv);
