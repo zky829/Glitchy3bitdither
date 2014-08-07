4124ee929a58045304b4ef23fca0bf36dde8a5d7
JKirchartz <JKirchartz@gmail.com>
2014-08-07 09:56:34 -0400

    try `Array.prototype.call` for sorting, instead of making a whole new array.

--- GlitchyDither.js
+++ GlitchyDither.js
@@ -540,10 +540,10 @@ function slicesort(imageData) {
         height = imageData.height,
         data = new Uint32Array(imageData.data.buffer),
         mm = slice_range(width,height),
-        cut = Array.apply([], data.subarray(mm[0], mm[1])),
+        cut = data.subarray(mm[0], mm[1]),
 	offset = Math.floor((Math.random() * (width * height))-cut.length);
     console.log("slicesort", data, mm, cut, offset);
-    cut.sort(numericSort);
+    Array.prototype.sort.call(cut, numericSort);
     data.set(cut, offset);
     imageData.data.set(data);
     return imageData;
