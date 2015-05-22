6f32cc1318618a16f8da9ef43b6816137185e227
Joel Kirchartz <jkirchartz@gmail.com>
2015-05-21 22:54:53 -0400

    test function randomization

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 var gleech = require('./lib/gleech').gleech;
-
+var functions = [
+  'pixelate',
+  'fractalGhosts',
+  'fractalGhosts2',
+  'fractalGhosts3',
+  'slice',
+  'dither8bit',
+  'shortSort',
+  'sortA',
+  'sortB',
+  'sort',
+  'sliceSort',
+  'sortRows',
+  'rgbGlitch',
+  'invert',
+  'anySort',
+  'anyShortSort',
+  'redShift',
+  'greenShift',
+  'blueShift',
+  'superShift',
+  'ditherRandom',
+  'ditherRandomColor',
+  'ditherBitshift'
+];
 gleech('./examples/Lenna.png', function() {
-  this.pixelate();
+  var i = ~~(functions.length * Math.random());
+  this[functions[i]]();
   this.render(function() {
     this.save('./examples/output.png');
   });
