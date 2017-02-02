18e7c19fc1fd005bb5d14d1fb0a5fef1f434274c
Joel Kirchartz <jkirchartz@gmail.com>
2017-02-01 23:52:20 -0500

    create Jimp project, begin turning functions into Jimp prototypes -- need to figure out a way to test this (probably by rewriting GlitchyDither.js & some html)

---
+++ gleech.js
@@ -0,0 +1,50 @@
+/*
+ * gleech.js
+ * Copyright (C) 2017 jkirchartz <me@jkirchartz.com>
+ *
+ * Distributed under terms of the NPL (Necessary Public License) license.
+ */
+var Jimp = require('jimp');
+
+Jimp.prototype.dither8Bit = function dither8Bit(size) {
+	if ("number" != typeof size)
+		return throwError.call(this, "size must be a number", cb);
+	if (size < 2)
+		return throwError.call(this, "size must be greater than 1", cb);
+	var width = this.bitmap.width,
+			height = this.bitmap.height,
+			data = this.bitmap.data,
+			sum_r, sum_g, sum_b, avg_r, avg_g, avg_b;
+
+	for (var y = 0; y < height; y += size) {
+		for (var x = 0; x < width; x += size) {
+			sum_r = 0;
+			sum_g = 0;
+			sum_b = 0;
+			var s_y, s_x, i;
+			for (s_y = 0; s_y < size; s_y++) {
+				for (s_x = 0; s_x < size; s_x++) {
+					i = 4 * (width * (y + s_y) + (x + s_x));
+					sum_r += data[i];
+					sum_g += data[i + 1];
+					sum_b += data[i + 2];
+				}
+			}
+			avg_r = (sum_r / (size * size)) > 127 ? 0xff : 0;
+			avg_g = (sum_g / (size * size)) > 127 ? 0xff : 0;
+			avg_b = (sum_b / (size * size)) > 127 ? 0xff : 0;
+			for (s_y = 0; s_y < size; s_y++) {
+				for (s_x = 0; s_x < size; s_x++) {
+					i = 4 * (width * (y + s_y) + (x + s_x));
+					data[i] = avg_r;
+					data[i + 1] = avg_g;
+					data[i + 2] = avg_b;
+				}
+			}
+		}
+	}
+	this.bitmap.data = new Buffer(data);
+	if (isNodePattern(cb)) return cb.call(this, null, this);
+	else return this;
+};
+
